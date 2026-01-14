001b0b2fa23f4f8b541cb6e405748edf899e4fd7
Mujammil Khan <mujammilk476@gmail.com>
2026-01-14 16:57:35 +0530

    debug js load

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+alert("SCRIPT LOADED");
+
 function closePopup() {
+  alert("CLOSE CLICKED");
   document.getElementById("overlay").style.display = "none";
   document.getElementById("contactBox").style.display = "none";
 }
